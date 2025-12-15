eab1a256217caa0e1a033d0f0fbcf70221d5b302
luis <luis.gcodes@gmail.com>
2025-12-15 17:28:43 -0500

    test run backend

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,15 @@
 from flask import Flask, request, render_template
-import diffuser
+import diffusers
+
+app = Flask(__name__)
+
+# @app.route('/image', methods=['POST'])
+# def generate_image():
+#     pass
+
 def main():
     print("Hello from backend!")
-
+    app.run(host='0.0.0.0', port=3077)
 
 if __name__ == "__main__":
     main()
-
-app = Flask(__name__)
-@app.route('/image', methods=['POST'])
-def generate_image():
-    data = request.json
-    prompt = data.get('prompt', '')
-    negative_prompt = data.get('negative_prompt', '')
-    image = diffuser.generate_image(prompt, negative_prompt)
-    return render_template('image.html', image=image)
